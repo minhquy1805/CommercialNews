d13e11bc5eb07b006b664a20206d9e3277d4f776
Minh Quy <minhquy073@gmail.com>
2026-03-19 20:02:11 +0700

    Add worker processing for pending outbox messages

--- src/hosts/CommercialNews.Worker/Program.cs
+++ src/hosts/CommercialNews.Worker/Program.cs
@@ -1,7 +1,15 @@
-using CommercialNews.Worker;
+using CommercialNews.Worker.HostedServices;
+using CommercialNews.Worker.Messaging.Outbox.Ports;
+using CommercialNews.Worker.Messaging.Outbox.Sql;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<WorkerSqlConnectionFactory>();
+
+builder.Services.AddScoped<IOutboxMessageReader, SqlOutboxMessageReader>();
+builder.Services.AddScoped<IOutboxMessageStateRepository, SqlOutboxMessageStateRepository>();
+
+builder.Services.AddHostedService<OutboxPollingService>();
 
 var host = builder.Build();
 host.Run();
