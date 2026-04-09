1c2d8581c4ebd30ba11d89ad7a7926b5440f4ba1
Minh Quy <minhquy073@gmail.com>
2026-04-09 23:05:33 +0700

    refactor worker setup and remove legacy outbox email pipeline

--- src/hosts/CommercialNews.Worker/Program.cs
+++ src/hosts/CommercialNews.Worker/Program.cs
@@ -1,33 +1,8 @@
-using CommercialNews.Worker.HostedServices;
-using CommercialNews.Worker.Messaging.Authorization.Dispatching;
-using CommercialNews.Worker.Messaging.Authorization.Ports;
-using CommercialNews.Worker.Messaging.Email.Configuration;
-using CommercialNews.Worker.Messaging.Email.Dispatching;
-using CommercialNews.Worker.Messaging.Email.Ports;
-using CommercialNews.Worker.Messaging.Email.Sending;
-using CommercialNews.Worker.Messaging.Email.Sql;
-using CommercialNews.Worker.Messaging.Outbox.Dispatching;
-using CommercialNews.Worker.Messaging.Outbox.Ports;
-using CommercialNews.Worker.Messaging.Outbox.Sql;
+using CommercialNews.Worker.CompositionRoot;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.Configure<MailSettings>(
-    builder.Configuration.GetSection("MailSettings"));
-
-builder.Services.AddSingleton<WorkerSqlConnectionFactory>();
-
-builder.Services.AddScoped<IOutboxMessageReader, SqlOutboxMessageReader>();
-builder.Services.AddScoped<IOutboxMessageStateRepository, SqlOutboxMessageStateRepository>();
-
-builder.Services.AddScoped<IEmailDeliveryRepository, SqlEmailDeliveryRepository>();
-builder.Services.AddScoped<IWorkerEmailSender, MailKitEmailSender>();
-// builder.Services.AddScoped<IWorkerEmailSender, WorkerEmailSender>();
-builder.Services.AddScoped<IOutboxEventEmailDispatcher, OutboxEventEmailDispatcher>();
-builder.Services.AddScoped<IAuthorizationOutboxEventDispatcher, AuthorizationOutboxEventDispatcher>();
-builder.Services.AddScoped<IOutboxEventDispatcher, OutboxEventDispatcher>();
-
-builder.Services.AddHostedService<OutboxPollingService>();
+builder.Services.AddWorkerModules(builder.Configuration);
 
 var host = builder.Build();
 host.Run();
