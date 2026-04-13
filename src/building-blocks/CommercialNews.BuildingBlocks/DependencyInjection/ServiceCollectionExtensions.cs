1b87134b30bef038fba847d3637cf8b42317d5a1
Minh Quy <minhquy073@gmail.com>
2026-04-13 20:46:55 +0700

    wire notification modules into api and worker hosts

--- src/building-blocks/CommercialNews.BuildingBlocks/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/building-blocks/CommercialNews.BuildingBlocks/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
+using CommercialNews.BuildingBlocks.Abstractions.Identifiers;
+using CommercialNews.BuildingBlocks.Abstractions.Time;
+using CommercialNews.BuildingBlocks.Identifiers;
 using CommercialNews.BuildingBlocks.Persistence.Sql;
+using CommercialNews.BuildingBlocks.Time;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,7 +10,7 @@ namespace CommercialNews.BuildingBlocks.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddBuildingBlocksSql(
+    public static IServiceCollection AddBuildingBlocks(
         this IServiceCollection services,
         IConfiguration configuration)
     {
@@ -15,6 +19,9 @@ public static class ServiceCollectionExtensions
 
         services.Configure<SqlOptions>(configuration.GetSection("Sql"));
         services.AddSingleton<ISqlConnectionFactory, SqlConnectionFactory>();
+
+        services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
+        services.AddSingleton<IPublicIdGenerator, UlidPublicIdGenerator>();
 
         return services;
     }
