b1d7b6ad9ebac33a4a5edba77c5eb995bacee03e
Minh Quy <minhquy073@gmail.com>
2026-05-01 18:23:18 +0700

    refactor(authz): remove legacy authorization outbox code

--- src/modules/Authorization/Authorization.Application/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/modules/Authorization/Authorization.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,3 @@
-using Authorization.Application.Outbox.Runtime;
 using Authorization.Application.UseCases.Permissions.ActivatePermission;
 using Authorization.Application.UseCases.Permissions.CreatePermission;
 using Authorization.Application.UseCases.Permissions.DeactivatePermission;
@@ -17,7 +16,6 @@ using Authorization.Application.UseCases.Roles.UpdateRole;
 using Authorization.Application.UseCases.UserRoles.AssignRoleToUser;
 using Authorization.Application.UseCases.UserRoles.GetUserRoles;
 using Authorization.Application.UseCases.UserRoles.RevokeRoleFromUser;
-using CommercialNews.BuildingBlocks.Outbox.Runtime;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Authorization.Application.DependencyInjection;
@@ -50,9 +48,6 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IGetPermissionRolesUseCase, GetPermissionRolesUseCase>();
 
         services.AddScoped<IGetUserEffectivePermissionsUseCase, GetUserEffectivePermissionsUseCase>();
-
-        services.AddScoped<IOutboxBatchProcessor, AuthorizationOutboxBatchProcessor>();
-        services.AddScoped<IOutboxMessageProcessor, AuthorizationOutboxMessageProcessor>();
 
         return services;
     }
--- src/modules/Authorization/Authorization.Infrastructure/Outbox/AuthorizationOutboxDefaults.cs
+++
@@ -1,6 +0,0 @@
-namespace Authorization.Infrastructure.Outbox;
-
-internal static class AuthorizationOutboxDefaults
-{
-    public const int DefaultPriority = 5;
-}
