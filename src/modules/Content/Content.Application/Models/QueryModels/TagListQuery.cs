fdf293651d1866db2cdd7236efa3f368f384a0c3
Minh Quy <minhquy073@gmail.com>
2026-05-12 17:13:38 +0700

    refactor(content): align query models with v1 schema

--- src/modules/Content/Content.Application/Models/QueryModels/TagListQuery.cs
+++ src/modules/Content/Content.Application/Models/QueryModels/TagListQuery.cs
@@ -2,11 +2,14 @@ namespace Content.Application.Models.QueryModels
 {
     public sealed class TagListQuery
     {
-        public int Page { get; init; }
-        public int PageSize { get; init; }
+        public int Page { get; init; } = 1;
+
+        public int PageSize { get; init; } = 20;
 
         public string? Keyword { get; init; }
+
         public bool? IsActive { get; init; }
+
         public bool IsDeleted { get; init; }
 
         public string Sort { get; init; } = "name";
